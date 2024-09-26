0c5a1feebdb11052cf7ecebc09782aaa696af4e1
erik <erik-r-11@hotmail.de>
2024-09-26 17:05:40 +0200

    logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
-import bootstrap from 'bootstrap'
+console.log("loaded");
+
+// Fragen
+const questions = ["Frage 2", "Frage 3", "Frage 4"];
+const question = document.getElementById("q");
+function QuestionHandler(q) {
+    console.log(q.id);
+
+    question.innerText = questions.shift();
+}
+
